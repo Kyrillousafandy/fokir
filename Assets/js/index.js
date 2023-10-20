2faa7d1c671e685617312996fdddcc2d8538e788
kyrillosafandy <kyrillosafandy@gmail.com>
2023-10-20 21:21:05 +0300

    initial commit

---
+++ Assets/js/index.js
@@ -0,0 +1,16 @@
+
+/*==================strat typed plugin=============================*/
+var typed = new Typed('#element', {
+    strings: [
+      'Professional web Developer',
+      'Professional web Designer',
+    ],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true,
+    loopCount: Infinity,
+    showCursor: false,
+  });
+/*==================End typed plugin=============================*/
+
+
